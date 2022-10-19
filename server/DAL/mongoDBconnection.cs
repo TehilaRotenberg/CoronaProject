db6e1b6cc59bf1aeaeb66b2640999c9cd261bda1
TehilaRotenberg <89943199+TehilaRotenberg@users.noreply.github.com>
2022-10-20 02:09:51 +0300

    mongodb_connection

---
+++ server/DAL/mongoDBconnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DAL
+{
+    public class mongoDBconnection
+    {
+        const string DATABASE_NAME = "CoronaProject";
+        public MongoClient connection =new MongoClient( @"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+        public MongoDatabaseBase database { get; set; }
+        public IMongoCollection<BsonDocument> collection { get; set; }
+       
+        public mongoDBconnection(string collection_name)
+        {
+            this.database=(MongoDatabaseBase)connection.GetDatabase(DATABASE_NAME);
+            this.collection = collection = database.GetCollection<BsonDocument>(collection_name);
+        }
+    }
+
+ 
+}
