c7479898d8c17b71634f93ba7494ec04b3394aa6
TehilaRotenberg <89943199+TehilaRotenberg@users.noreply.github.com>
2022-10-20 08:54:06 +0300

    upload_dbconnection_GET&Create_method

--- server/DAL/mongoDBconnection.cs
+++ server/DAL/mongoDBconnection.cs
@@ -8,18 +8,38 @@ using MongoDB.Driver;
 
 namespace DAL
 {
-    public class mongoDBconnection
+    public abstract class mongoDBconnection<T>
     {
         const string DATABASE_NAME = "CoronaProject";
         public MongoClient connection =new MongoClient( @"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
         public MongoDatabaseBase database { get; set; }
         public IMongoCollection<BsonDocument> collection { get; set; }
+
+        List<BsonDocument> documents;
+        List<T> list;
        
         public mongoDBconnection(string collection_name)
         {
             this.database=(MongoDatabaseBase)connection.GetDatabase(DATABASE_NAME);
             this.collection = collection = database.GetCollection<BsonDocument>(collection_name);
+            List<BsonDocument> documents = collection.Find(new BsonDocument()).ToList();
         }
+
+        //Create
+        public void Create(T obj)
+        {
+            collection.InsertOne(CreatBsonDocument(obj));
+        }
+        
+        //Read
+       public abstract List<T> Get();
+
+        //Update
+        //Delete
+
+
+        public abstract BsonDocument CreatBsonDocument(T obj);
+
     }
 
  
